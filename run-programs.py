f07d9f44da27406e18f1e8e829f309acd286be2b
Harman Hayer <hhhayer@gmail.com>
2021-06-07 13:31:03 -0700

    bug fix: process would stop at chrome

--- run-programs.py
+++ run-programs.py
@@ -1,10 +1,12 @@
 import subprocess;
 
+
+
 print('Opening Google Chrome');
-subprocess.call('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe');
-subprocess.call('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe');
+subprocess.Popen('"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"');
+subprocess.Popen('"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"');
 print('Opening Notepad++');
-subprocess.call('C:\\Program Files\\Notepad++\\notepad++.exe');
+subprocess.Popen('"C:\\Program Files\\Notepad++\\notepad++.exe"');
 print('Opening MongoDB Compass');
 subprocess.call('C:\\Users\\hhayer\\AppData\\Local\\MongoDBCompass\\MongoDBCompass.exe');
 print('Opening PomoDoneApp');
@@ -12,8 +14,8 @@ subprocess.call('C:\\Users\\hhayer\\AppData\\Local\\PomoDoneApp\\PomoDoneApp.exe
 print('Opening Insomnia');
 subprocess.call('C:\\Users\\hhayer\\AppData\\Local\\insomnia\\Insomnia.exe');
 print('Opening Visual Studio Code');
-subprocess.call('"C:\\Users\\hhayer\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"');
+subprocess.Popen('"C:\\Users\\hhayer\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"');
 print('Opening Notion');
-subprocess.call('C:\\Users\\hhayer\\AppData\\Local\\Programs\\Notion\\Notion.exe');
+subprocess.Popen('C:\\Users\\hhayer\\AppData\\Local\\Programs\\Notion\\Notion.exe');
 
 #run a list of programs
